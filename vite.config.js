85d4e389d34aa4f4e24fcfb2cba4e07ebbb718c6
TanKittipat <kittipat12tan@gmail.com>
2025-03-18 15:43:39 +0700

    Remove base path and build output directory from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my-bmi-project/",
-  build: {
-    outDir: "dist",
-  },
 });
